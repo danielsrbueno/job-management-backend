3753d6b4f80e4358ab77e9e76baa9d6f07fead57
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-06-21 19:09:15 -0300

    feat: add error message dto

--- src/main/java/com/danbueno/job_management/exceptions/ErrorMessageDTO.java
+++ src/main/java/com/danbueno/job_management/exceptions/ErrorMessageDTO.java
@@ -1,4 +1,11 @@
 package com.danbueno.job_management.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ErrorMessageDTO {
+  private String message;
+  private String field;
 }

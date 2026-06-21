f34bc7f1d0ad4800cf774d9d7c6eb39574bb3545
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-06-21 19:05:50 -0300

    feat(candidate): add candidate entity

--- src/main/java/com/danbueno/job_management/modules/candidate/CandidateEntity.java
+++ src/main/java/com/danbueno/job_management/modules/candidate/CandidateEntity.java
@@ -1,4 +1,48 @@
 package com.danbueno.job_management.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private String name;
+
+  @NotBlank
+  @Pattern(regexp = "\\S+", message = "O campo username não deve conter espaço")
+  private String username;
+
+  @Email(message = "O campo email deve contem um e-mail válido")
+  private String email;
+
+  @Length(min = 8, max = 100, message = "O campo senha deve conter entre 8 e 100 caracteres")
+  private String password;
+
+  private String description;
+
+  private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
 }

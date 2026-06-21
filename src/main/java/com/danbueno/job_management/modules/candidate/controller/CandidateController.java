898f21fced1157e1b3c86effe6ef49bbfe3952cd
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-06-21 19:06:50 -0300

    feat(candidate): add create candidate controller

--- src/main/java/com/danbueno/job_management/modules/candidate/controller/CandidateController.java
+++ src/main/java/com/danbueno/job_management/modules/candidate/controller/CandidateController.java
@@ -1,4 +1,30 @@
 package com.danbueno.job_management.modules.candidate.controller;
 
+import com.danbueno.job_management.modules.candidate.CandidateEntity;
+import com.danbueno.job_management.modules.candidate.useCases.CreateCandidateUseCase;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
 public class CandidateController {
+
+  @Autowired
+  private CreateCandidateUseCase createCandidateUseCase;
+
+  @PostMapping("/")
+  public ResponseEntity<Object> create (@Valid @RequestBody CandidateEntity candidateEntity) {
+    try {
+      var result = this.createCandidateUseCase.execute(candidateEntity);
+      return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+  }
 }

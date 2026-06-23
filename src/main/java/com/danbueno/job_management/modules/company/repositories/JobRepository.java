a4b6bc6b9fdf437a5b063cd562702b3252fa09d6
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-06-23 18:08:47 -0300

    feat(job): add job repository

---
+++ src/main/java/com/danbueno/job_management/modules/company/repositories/JobRepository.java
@@ -0,0 +1,8 @@
+package com.danbueno.job_management.modules.company.repositories;
+
+import com.danbueno.job_management.modules.company.entities.JobEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {}

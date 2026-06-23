8e700cef0fcb1931a871ac1e58537e0fe69a6fcb
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-06-23 18:07:06 -0300

    feat(job): add job entity

---
+++ src/main/java/com/danbueno/job_management/modules/company/entities/JobEntity.java
@@ -0,0 +1,40 @@
+package com.danbueno.job_management.modules.company.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "job")
+@Data
+public class JobEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @NotBlank(message = "O campo descrição é obrigatório")
+  private String description;
+  private String benefits;
+
+  @NotBlank(message = "O campo nível é obrigatório")
+  private String level;
+
+  @ManyToOne
+  @JoinColumn(name = "company_id", insertable = false, updatable = false)
+  private CompanyEntity companyEntity;
+
+  @Column(name = "company_id", nullable = false)
+  private UUID companyId;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+}

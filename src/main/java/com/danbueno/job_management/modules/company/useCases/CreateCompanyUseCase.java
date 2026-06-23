245d216e72590d6a239deedf293f9814f75832d6
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-06-23 18:09:09 -0300

    feat(company): add create company use case

---
+++ src/main/java/com/danbueno/job_management/modules/company/useCases/CreateCompanyUseCase.java
@@ -0,0 +1,28 @@
+package com.danbueno.job_management.modules.company.useCases;
+
+import com.danbueno.job_management.exceptions.UserFoundException;
+import com.danbueno.job_management.modules.company.entities.CompanyEntity;
+import com.danbueno.job_management.modules.company.repositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCompanyUseCase {
+
+  @Autowired
+  private CompanyRepository companyRepository;
+
+  public CompanyEntity execute (CompanyEntity companyEntity) {
+
+    this.companyRepository
+        .findByUsernameOrEmail(
+            companyEntity.getUsername(),
+            companyEntity.getEmail()
+        )
+        .ifPresent((user) -> {
+          throw new UserFoundException();
+        });
+
+    return this.companyRepository.save(companyEntity);
+  }
+}

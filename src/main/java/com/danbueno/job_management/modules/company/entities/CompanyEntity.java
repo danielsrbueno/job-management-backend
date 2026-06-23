960c563a83a0c4e1253b77ba830d3e0e81e98d6b
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-06-23 18:06:28 -0300

    feat(company): add company entity

---
+++ src/main/java/com/danbueno/job_management/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,45 @@
+package com.danbueno.job_management.modules.company.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @NotBlank()
+  @Pattern(regexp = "\\S+", message = "O campo username não deve conter espaço")
+  private String username;
+
+  @Email(message = "O campo email deve contem um e-mail válido")
+  private String email;
+
+  @Length(min = 8, max = 100, message = "O campo senha deve conter entre 8 e 100 caracteres")
+  private String password;
+
+  private String website;
+  private String name;
+  private String description;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+}

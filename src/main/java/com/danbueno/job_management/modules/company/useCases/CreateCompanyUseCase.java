ddad1578c4848c873cc361fdb15759ff1197c762
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-06-29 22:29:03 -0300

    feat(auth-company): add auth company use case

--- src/main/java/com/danbueno/job_management/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/com/danbueno/job_management/modules/company/useCases/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import com.danbueno.job_management.exceptions.UserFoundException;
 import com.danbueno.job_management.modules.company.entities.CompanyEntity;
 import com.danbueno.job_management.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,9 @@ public class CreateCompanyUseCase {
 
   @Autowired
   private CompanyRepository companyRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
   public CompanyEntity execute (CompanyEntity companyEntity) {
 
@@ -22,6 +26,9 @@ public class CreateCompanyUseCase {
         .ifPresent((user) -> {
           throw new UserFoundException();
         });
+
+    var password = passwordEncoder.encode(companyEntity.getPassword());
+    companyEntity.setPassword(password);
 
     return this.companyRepository.save(companyEntity);
   }
